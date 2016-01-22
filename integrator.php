50f1a549ca008280c3989193124631bbec17332a
Igor Bobko <limit-speed@yandex.ru>
2016-01-22 17:06:19 +0300

    Signed-off-by: Igor Bobko <limit-speed@yandex.ru>

--- integrator.php
+++ integrator.php
@@ -1,39 +1,41 @@
 <?php
 
-//not_paid
-$postdata = http_build_query(
-    array(
-		'order[paymentStatus]' => 'not_paid',
-		'by'=>'externalId',
-		'site'=>'www-online-krasota-ru'
-		)
-);
+class Integrator {
+	private $site = 'www-online-krasota-ru';
+	function __construct() {
 
-
-
-
-$opts = array('http' => array(
-                'proxy' => 'tcp://localhost:41009',
-				'request_fulluri'=>true,
+	}
+	
+	private function getContext($postdata) {
+		$opts = array('http' => array(
+				'proxy' => 'tcp://localhost:41009',
+				'request_fulluri' => true,
 				'method'  => 'POST',
-				'content' => $postdata,
-				'header'  => 'Content-type: application/x-www-form-urlencoded'
+				'content' =>  $postdata,
+				'header' => 'Content-type: application/x-www-form-urlencoded'
 				));
+		return stream_context_create($opts);
+	}
 
-$context = stream_context_create($opts); 
-
-
-
-$content = file_get_contents("https://online-krasota.retailcrm.ru/api/v3/orders/781/edit?apiKey=DNtmSFCVbZWMQ1GXRJ82cBpRbbtPSA8n",false,$context);
-//$content = file_get_contents("https://online-krasota.retailcrm.ru/api/v3/orders?apiKey=DNtmSFCVbZWMQ1GXRJ82cBpRbbtPSA8n",false,$context);
-
-
-$result = json_decode($content);
+	public function changeStatus($orderId,$by,$status) {
+		$postdata = http_build_query(
+			array(
+				'order[paymentStatus]' => $status,
+				'by' => $by,
+				'site' => $this->site
+				)
+		);
+		$content = file_get_contents("https://online-krasota.retailcrm.ru/api/v3/orders/".$orderId."/edit?apiKey=DNtmSFCVbZWMQ1GXRJ82cBpRbbtPSA8n",false,$this->getContext($postdata));
+		return json_decode($content);
+	}
+	
+	public function listOrder() {
+		//$content = file_get_contents("https://online-krasota.retailcrm.ru/api/v3/orders?apiKey=DNtmSFCVbZWMQ1GXRJ82cBpRbbtPSA8n",false,$context);
+	}
+}
+		//not_paid
+$integrator = new Integrator();
 echo "<pre>";
-print_r($result);
+print_r($integrator->changeStatus(781,'externalId','not_paid'));
 echo "</pre>";
 
-
-//include "api-client-php-master/lib/ApiClient.php";
-
-//$client = 

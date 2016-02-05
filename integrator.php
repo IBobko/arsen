463d1bed649966953506c630fbfc5ab6c2be50eb
Igor Bobko <limit-speed@yandex.ru>
2016-02-05 18:52:08 +0300

    Last version)))

--- integrator.php
+++ integrator.php
@@ -1,10 +1,39 @@
 <?php
+set_time_limit(300);
 
-class Integrator {
-	private $site = 'www-online-krasota-ru';
+class MoySklad {
+	private $username = "sklad@online_krasota";
+	private $password = "1march1985";	
+
 	function __construct() {
 
 	}
+
+	public function customerOrder() {
+		$params = array('http' => array('proxy' => 'tcp://localhost:41009','header'  => "Authorization: Basic " . base64_encode("{$this->username}:{$this->password}")));
+		$context = stream_context_create($params);
+		$data = file_get_contents("https://online.moysklad.ru/api/remap/1.0/entity/customerOrder", false, $context);
+		return json_decode($data);
+	}
+
+	public function getOrdersSize() {
+		$params = array('http' => array('proxy' => 'tcp://localhost:41009','header'  => "Authorization: Basic " . base64_encode("{$this->username}:{$this->password}")));
+		$context = stream_context_create($params);		
+		$result = json_decode(file_get_contents("https://online.moysklad.ru/api/remap/1.0/entity/customerOrder?offset=100000",false,$context));		
+		return $result->metaTO->size;
+	}
+
+	public function getLastHundred($size) {
+		$params = array('http' => array('proxy' => 'tcp://localhost:41009','header'  => "Authorization: Basic " . base64_encode("{$this->username}:{$this->password}")));
+		$context = stream_context_create($params);		
+		$result = json_decode(file_get_contents("https://online.moysklad.ru/api/remap/1.0/entity/customerOrder?offset=".($size-50)."&limit=50",false,$context));	
+		return $result;
+	}
+}
+
+
+class Integrator {
+	function __construct() {}
 	
 	private function getContext($postdata) {
 		$opts = array('http' => array(
@@ -17,25 +46,86 @@ class Integrator {
 		return stream_context_create($opts);
 	}
 
-	public function changeStatus($orderId,$by,$status) {
+	public function changeStatus($orderId,$status,$date,$by,$site) {
 		$postdata = http_build_query(
 			array(
-				'order[paymentStatus]' => $status,
+				'order' => json_encode(array('paymentStatus' => $status,'customFields'=>array('date_cash'=>$date))),
 				'by' => $by,
-				'site' => $this->site
+				'site' => $site
 				)
 		);
 		$content = file_get_contents("https://online-krasota.retailcrm.ru/api/v3/orders/".$orderId."/edit?apiKey=DNtmSFCVbZWMQ1GXRJ82cBpRbbtPSA8n",false,$this->getContext($postdata));
 		return json_decode($content);
 	}
 	
-	public function listOrder() {
-		//$content = file_get_contents("https://online-krasota.retailcrm.ru/api/v3/orders?apiKey=DNtmSFCVbZWMQ1GXRJ82cBpRbbtPSA8n",false,$context);
+	public function listOrder($orderid) {
+		$opts = array('http' => array(
+				'proxy' => 'tcp://localhost:41009',
+				'request_fulluri' => true,
+				));
+		$context = stream_context_create($opts);
+		$content = file_get_contents("https://online-krasota.retailcrm.ru/api/v3/orders?apiKey=DNtmSFCVbZWMQ1GXRJ82cBpRbbtPSA8n&filter[number]=$orderid",false,$context);
+		return json_decode($content);
+	}
+
+	public function start() {
+		$sklad = new MoySklad();		
+		$orders = $sklad->getUpdatingOrders();
+		foreach($orders as $order) {
+			$this->changeStatus($order,'externalId','paid');
+			print_r($order);
+		}
+	}
+
+	public function t() {
+		$opts = array('http' => array(
+				'proxy' => 'tcp://localhost:41009',
+				'request_fulluri' => true,
+				));
+		$context = stream_context_create($opts);		
+		$content = file_get_contents("https://online-krasota.retailcrm.ru/api/v3/orders?apiKey=DNtmSFCVbZWMQ1GXRJ82cBpRbbtPSA8n&page=2",false,$context);
+		print_r(json_decode($content));
 	}
 }
-		//not_paid
+
 $integrator = new Integrator();
+
+$moySklad = new MoySklad();
+$size = $moySklad->getOrdersSize();
+
+$orders = $moySklad->getLastHundred($size);
+
 echo "<pre>";
-print_r($integrator->changeStatus(781,'externalId','not_paid'));
+//$integrator->t();
+//$integrator->changeStatus(27899,"not-paid","id","npcriz-sklad-ru");
+echo "</pre>";
+//exit();
+
+echo "<pre>";
+foreach($orders->rows as $value) {
+	
+//	print_r($integrator->listOrder($value->name)->orders);
+
+	$crmOrder = $integrator->listOrder($value->name)->orders[0];
+	//print_r($crmOrder);
+//	continue;
+	if ($value->applicable == 1) {
+		if ($value->payedSum == 0 && $crmOrder->paymentStatus != "not-paid") {
+			echo "Для заказа " . $crmOrder->id . " необходимо обновить статус. Сейчас у него {$crmOrder->paymentStatus}</br>";
+			echo "Обновляю<br/>";
+			print_r($integrator->changeStatus($crmOrder->id,"not-paid",0,"id",$crmOrder->site));
+			echo "Обновил";			
+		}
+	
+		if ($value->payedSum !=0 && $crmOrder->paymentStatus != "paid") {
+			echo "Для заказа " . $crmOrder->id . " необходимо обновить статус. Сейчас у него {$crmOrder->paymentStatus}</br>";
+			echo "Обновляю<br/>";
+			print_r($integrator->changeStatus($crmOrder->id,"paid","id",$value->updated,$crmOrder->site));
+			echo "Обновил";
+		}
+	}
+	
+	echo "<hr/>";
+}
 echo "</pre>";
 
